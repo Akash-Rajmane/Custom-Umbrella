a53780e71541255c798123e3d0fd0bd7773f87bb
Akash-Rajmane <akashrajmane23@gmail.com>
2023-06-05 22:43:08 +0530

    added event-delegation

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 let img = document.getElementById("img");
 let loader = document.getElementById("loader");
-let image = document.getElementById('brandLogo');
+let image = document.getElementById("brandLogo");
+let input = document.getElementById("file-upload");
+let label = document.getElementById("label");
 
 function hideLoaderShowImg() {
-    img.style.opacity = 1;
-    loader.style.opacity = 0;  
-    if(image.src.includes("index")){
-        image.style.opacity = 0;
-    }else{
-        image.style.opacity = 1;
-    }
-}
-
-function hideImageShowLoader(){
-    img.style.opacity = 0;
-    loader.style.opacity = 1;
+  img.style.opacity = 1;
+  loader.style.opacity = 0;
+  if (image.src.includes("index")) {
     image.style.opacity = 0;
-}
-
-function changeImageToBlue(){
-    hideImageShowLoader();
-    img.src = "./assets/Blue umbrella.png"; 
-    setTimeout(hideLoaderShowImg, 1000);
-    img.alt = "Blue Umbrella";
-}
-
-function changeImageToPink(){
-    hideImageShowLoader();
-    img.src = "./assets/Pink umbrella.png";
-    setTimeout(hideLoaderShowImg, 1000);
-    img.alt = "Pink Umbrella";
-}
-
-function changeImageToYellow(){
-    hideImageShowLoader();
-    img.src = "./assets/Yellow umbrella.png";
-    setTimeout(hideLoaderShowImg, 1000);
-    img.alt = "Yellow Umbrella"
-}
-
-function loadFile(event) {
-    image.src= URL.createObjectURL(event.target.files[0]);
+  } else {
     image.style.opacity = 1;
-};
+  }
+}
+
+function hideImageShowLoader() {
+  img.style.opacity = 0;
+  loader.style.opacity = 1;
+  image.style.opacity = 0;
+}
+
+function changeImage(event) {
+  let span = event.target.closest("span");
+
+  if (event.target.tagName !== "SPAN") return;
+  else changeSource(span);
+}
+
+function changeSource(span) {
+  hideImageShowLoader();
+  let id = span.getAttribute("id");
+  id === "dot1"
+    ? (img.src = "./assets/Blue umbrella.png")
+    : id === "dot2"
+    ? (img.src = "./assets/Pink umbrella.png")
+    : (img.src = "./assets/Yellow umbrella.png");
+
+  setTimeout(hideLoaderShowImg, 1000);
+}
+
+input.addEventListener("change", showFileName);
+
+function showFileName(event) {
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.style.opacity = 1;
+  let fileName = event.target.files[0].name;
+  label.textContent = fileName;
+}
